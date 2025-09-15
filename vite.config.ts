24934bca9a710cdcf5f4894898eed76341e84624
cherwood87 <cherwood87@gmail.com>
2025-09-15 09:39:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,3 +9,4 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
+ // test deploy
